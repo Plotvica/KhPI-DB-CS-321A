6c44be700ed10eef405438e08b67ddf5fb387b46
Plotvica <denya.boroday73wx@gmail.com>
2022-11-20 23:34:50 +0200

    1

--- ОперСис/Лабы/Laboratory work 4/Laboratory work 4.cpp
+++ ОперСис/Лабы/Laboratory work 4/Laboratory work 4.cpp
@@ -1,95 +1,82 @@
 ﻿#include<iostream>   
-#include<Windows.h>   
 #include<string>   
 using namespace std;
 
 int main() {
-    SetConsoleOutputCP(1251);
-    SetConsoleCP(1251);
-    int size = 100;
-    string stroka = "abc[qwe]k?[345]as[67]?r/?";//abc-k*-as-*r? 
-    string for_example = "lmnabcwkl4as71r?k0000";
-    string array_1 = "";
-    string array_2 = "";
-    string* array_3 = new string[size / 10];
+
+    system("chcp 1251"); system("cls");
+    
+    string primestring = "lmnabcwkl4as71r?k0000";
+    cout << primestring << endl;
+
+    string request = "abc[qwe]k?[345]as[67]?r/?";
+    cout << request << endl;
+   
+
+    string mask;
+    string result_string;
+    string* brackets = new string[10];
+
     int count = 0;
-    int count_for_string = 0;
-    int skobka = 2;
-    cout << stroka;
-    for (int i = 0; i < stroka.length(); i++) {
-        if (i != 0 && stroka[i - 1] != '/' && stroka[i] == '?') {
-            array_1 += '1';
-            array_2 += '0';
+    int count_breckets = 0;
+    int bracket = 2;
+
+    
+    for (int i = 0; i < request.length(); i++) {
+        if (i != 0 and request[i - 1] != '/' and request[i] == '?') {
+            mask += '1';
+            result_string += '0';
         }
-        else if (stroka[i] == '/') {
+        else if (request[i] == '/') {
             i++;
-            array_1 += '0';
-            array_2 += stroka[i];
+            mask += '0';
+            result_string += request[i];
         }
-        else if (stroka[i] == '[') {
-            array_1 += to_string(skobka);
-            array_2 += '0';
-            skobka++;
+        else if (request[i] == '[') {
+            mask += to_string(bracket);
+            result_string += '0';
+            bracket++;
             i++;
-            while (stroka[i] != ']') {
-                array_3[count_for_string] += stroka[i];
+            while (request[i] != ']') {
+                brackets[count_breckets] += request[i];
                 i++;
             }
-            count_for_string++;
+            count_breckets++;
         }
         else {
-            array_1 += '0';
-            array_2 += stroka[i];
+            mask += '0';
+            result_string += request[i];
         }
         count++;
     }
-    cout << endl;
-    for (int i = 0; i < count + 1; i++) {
-        cout << array_1[i];
-    }
-    cout << endl;
-    for (int i = 0; i < count + 1; i++) {
-        cout << array_2[i];
-    }
-    cout << endl;
-    for (int i = 0; i < count_for_string; i++) {
-        cout << array_3[i] << endl;
-    }
-    bool is = false;
-    bool is_1 = false;
-    int is_is = 0;
-    for (int i = 0; i < for_example.length(); i++)
-    {
-        if (for_example[i] == array_2[0]) {
-            is = true;
-            for (int j = 0, k = i; j < count; j++, k++) {
-                if (array_2[j] != '0') {
-                    cout << for_example[k] << "\t" << array_2[j] << endl;
-                    if (array_2[j] != for_example[k]) {
-                        is = false;
-                        break;
-                    }
+
+    /*cout << endl;
+    for (int i = 0; i < count_breckets; i++) cout << brackets[i] << endl;*/
+
+    cout << endl << mask << endl;
+   
+
+    cout << endl << result_string << endl << endl;
+
+    count_breckets = 0;
+
+    for (int i = 0; i < primestring.length(); i++)
+        if (primestring[i] == result_string[0])
+            for (int j = 0; j < count; j++, i++)
+                if (result_string[j] != '0' ) {
+                    if (result_string[j] != primestring[i]) break;
+                    else cout << primestring[i] << "\t" << result_string[j] << endl;
                 }
                 else {
-                    if (array_1[j] != '1') {
-                        int number = (array_1[j] - '0') - 2;
-                        cout << number << endl;
-                        for (int l = 0; l < array_3[number].length(); l++) {
-                            if (array_3[number][l] == for_example[k]) {
-                                cout << for_example[k] << "\t" << array_3[number][l] << endl;
-                                is_is = 1;
+                    if (mask[j] != '1') {
+                        for (int l = 0; l < brackets[count_breckets].length(); l++)
+                            if (brackets[count_breckets][l] == primestring[i]) {
+                                cout << primestring[i] << "\t" << brackets[count_breckets][l] << endl;
+                                count_breckets++;
+                                break;
                             }
-                        }
                     }
+                    else cout << primestring[i] << "\t" << result_string[j] << endl;   
                 }
-            }
-            if (is && is_is == 1) {
-                cout << "Даний рядок є" << endl;
-                is_1 = true;
-            }
-        }
-    }
-    if (!is_1)  cout << "Даного рядка немає в рядку " << endl;
-    
 
 }
